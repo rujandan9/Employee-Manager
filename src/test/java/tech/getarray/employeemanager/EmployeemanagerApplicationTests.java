c8a159ce2a98281da003932b402e2256eb25479a
Dan <rujandan9@gmail.com>
2025-03-27 23:57:44 +0200

    small improvements

--- src/test/java/tech/getarray/employeemanager/EmployeemanagerApplicationTests.java
+++ src/test/java/tech/getarray/employeemanager/EmployeemanagerApplicationTests.java
@@ -3,11 +3,11 @@ package tech.getarray.employeemanager;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class EmployeemanagerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
